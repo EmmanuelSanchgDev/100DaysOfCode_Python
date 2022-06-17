ce1ef5b33609b9327fb6d31bfc9d6f9bb4df196a
EmmanuelSanchg <emmanuelsanchgdev@gmail.com>
2022-06-17 12:47:59 -0700

    projecto1

---
+++ Day1/project1.py
@@ -0,0 +1,5 @@
+print( "Bienvenid@ a Elegir el nombre de tu Nueva Banda" )
+name_city = input("en que ciudad naciste?\n")
+name_pet = input("como se llama tu mascota?\n")
+name_band = name_city + " " + name_pet
+print("un Nombre genial para tu banda seria: " + name_band)
